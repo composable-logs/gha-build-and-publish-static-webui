457981e3d5f880747afb49d3f44bef1ad9476595
Matias Dahl <matias.dahl@iki.fi>
2022-10-22 12:51:51 +0300

    test main script

--- docker/gha-entrypoint.sh
+++ docker/gha-entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "hello!!"
+
+echo "PWD"
+pwd
+
+echo "ls"
+ls -la .
+
+#---
+
+static_builder \
+    --github_repository $GITHUB_REPOSITORY \
+    --output_dir /github_workspace/www-root/pipeline-artifacts/ \
+    --output_static_data_json /github_workspace/www-root/ui_static_data.json;
