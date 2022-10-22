724d35962d3d6d0b472ac2cc286bfcaae51c5d0a
Matias Dahl <matias.dahl@iki.fi>
2022-10-22 15:54:31 +0300

    entrypoint debug

--- docker/gha-entrypoint.sh
+++ docker/gha-entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+set -eux
 
-echo "hello!!"
-
-echo "PWD"
+echo "--- pwd -----"
 pwd
 
-echo "ls"
+echo "--- ls ---"
 ls -la .
 
-#---
+echo "-------------------------------------------------------"
+pip freeze
 
+echo "-------------------------------------------------------"
 static_builder \
     --github_repository $GITHUB_REPOSITORY \
     --output_dir /github_workspace/www-root/pipeline-artifacts/ \
     --output_static_data_json /github_workspace/www-root/ui_static_data.json;
+
+echo "--- done ---"
