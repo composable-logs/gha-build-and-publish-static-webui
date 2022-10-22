e5bbe20fce2c7a7ca69865f0d5b748f3908e7865
Matias Dahl <matias.dahl@iki.fi>
2022-10-22 16:49:14 +0300

    debug re pip install

--- docker/gha-entrypoint.sh
+++ docker/gha-entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 echo "-------------------------------------------------------"
-echo " - GITHUB_REPOSITORY              : $GITHUB_REPOSITORY "
-echo " - PYNB_DAG_RUNNER_DEPENDENCY     : $GITHUB_REPOSITORY "
+echo " - GITHUB_REPOSITORY              : ${GITHUB_REPOSITORY}"
+echo " - PYNB_DAG_RUNNER_DEPENDENCY     : ${INPUT_A_PARAMETER}"
+echo " - pynb_dag_runner_ref            : ${INPUT_PYNB_DAG_RUNNER_REF}"
 echo "-------------------------------------------------------"
 
 set -eux
 
-pip install --user pynb-dag-runner-snapshot
+whoami
 
+cd /home/host_user/
+
+pip install --no-cache-dir --user ${INPUT_PYNB_DAG_RUNNER_REF}
 
 echo "-------------------------------------------------------"
 
